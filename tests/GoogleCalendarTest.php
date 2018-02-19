b2f23b3a0687ae426c768a1b16ff4ad8dcf2fb88
Silvio <silvioq@gmail.com>
2018-02-19 19:56:52 -0300

    Test some methods on Google Calendar

---
+++ tests/GoogleCalendarTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Silvioq\GoogleCalendar\Tests;
+
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamWrapper;
+use org\bovigo\vfs\vfsStreamDirectory;
+use PHPUnit\Framework\TestCase;
+use Silvioq\GoogleCalendar\Converter\ConverterInterface;
+use Silvioq\GoogleCalendar\GoogleEventInterface;
+use Silvioq\GoogleCalendar\GoogleCalendar;
+
+class GoogleCalendarTest extends TestCase
+{
+    const CREDENTIAL_DATA = '{"access_token":"at","token_type":"Bearer","expires_in":3600,"refresh_token":"rt","created":1519068710}';
+    public function setUp()
+    {
+        vfsStreamWrapper::register();
+        vfsStreamWrapper::setRoot(new vfsStreamDirectory('testDir'));
+        file_put_contents(vfsStream::url('testDir/c.json'), self::CREDENTIAL_DATA);
+    }
+
+    public function testSimpleCreation()
+    {
+        $this->assertNotNull(new GoogleCalendar());
+
+        $mockClient = $this->getMockBuilder(\Google_Client::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $mockConv = $this->getMockBuilder(ConverterInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->assertNotNull(new GoogleCalendar([
+            'google_client' => $mockClient,
+        ]));
+
+        $this->assertNotNull(new GoogleCalendar([
+            'converter' => $mockConv,
+        ]));
+    }
+
+    /**
+     * @depends testSimpleCreation
+     */
+    public function testUrlBuilding()
+    {
+        $mockClient = $this->getMockBuilder(\Google_Client::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+
+        $mockClient->expects($this->once())
+            ->method('fetchAccessTokenWithAuthCode')
+            ->with('a')
+            ->willReturn(json_decode(self::CREDENTIAL_DATA, true))
+            ;
+
+        $calendar = new GoogleCalendar([
+            'credentials_path' => vfsStream::url('testDir/credentials.json')
+        ]);
+
+        $this->assertNotNull($calendar->buildClientWithAuthToken('a', $mockClient));
+        $this->assertSame(self::CREDENTIAL_DATA, file_get_contents(vfsStream::url('testDir/credentials.json')));
+    }
+
+    /**
+     * @depends testSimpleCreation
+     */
+    public function testListCalendars()
+    {
+        $mockClient = $this->getMockBuilder(\Google_Client::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockClient->expects($this->any())
+            ->method('getLogger')
+            ->willReturn(new \Psr\Log\NullLogger())
+            ;
+
+        $list = new \Google_Service_Calendar_CalendarList();
+        $list->setItems( [1,2,3] );
+
+        $mockClient->expects($this->once())
+            ->method('execute')
+            ->willReturn($list);
+
+        $calendar = new GoogleCalendar([
+            'google_client' => $mockClient,
+        ]);
+
+        $this->assertNotNull($items = $calendar->listCalendars());
+        $this->assertSame( [1,2,3], $items);
+    }
+}
+// vim:sw=4 ts=4 sts=4 et
